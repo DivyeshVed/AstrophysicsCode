c52beb18e5dc07dd3854bdb3a5ac1dd095fc3a94
Divyesh Ved <82323146+DivyeshVed@users.noreply.github.com>
2022-08-30 19:35:26 -0400

    Made slight experimental change

--- trials.py
+++ trials.py
@@ -2,6 +2,3 @@ import pickle
 
 data = pickle.load(open("/Users/rohanpunamiya/Dropbox (GaTech)/CygX2/goodValues.pkl","rb"))
 print(data.to_string())
-
-data1 = pickle.load(open("/Users/rohanpunamiya/Dropbox (GaTech)/CygX2/okayValues.pkl","rb"))
-print(data1.to_string())
