596863e32e95cefa5ad901e6478da5b6368303be
Divyesh Ved <82323146+DivyeshVed@users.noreply.github.com>
2022-09-30 12:50:06 -0400

    This code is full of bugs and needs to be cleaned, howver it shoul dbe operational.

--- QandSigTable.py
+++ QandSigTable.py
@@ -2,17 +2,23 @@ import pickle
 import pandas as pd
 
 
-
+# Loading data in from the Q_Significance_Values table.
 data = pickle.load(open("/Users/rohanpunamiya/Dropbox (GaTech)/CygX2/Q_Significance_Values.pkl","rb"))
+# Loading values from the dataTable table.
 data1 = pickle.load(open("/Users/rohanpunamiya/Desktop/AstrophysicsCode/dataTable.pkl","rb"))
-data = data.reset_index()
-data = data.drop('index',axis = 1)
-print(data)
+# Ressetting the index in the Q and Significance_Values table.
+# data = data.reset_index()
+# data = data.drop('index',axis = 1)
+# print(data)
+# Making column header list that will be used for the two tables formed in this code.
 columnHeader = ['OBSID','Frequency','Best Fit Model','RMS Value','Q Value','Significance']
+# Creating a dataframe that will hold all the good values that represent qpos.
 goodValues = pd.DataFrame(columns = columnHeader)
+# Creating a dataframe that will hold all the okay values that represent qpos.
 okayValues = pd.DataFrame(columns = columnHeader)
 # data.to_pickle("/Users/rohanpunamiya/Dropbox (GaTech)/CygX2/Q_Significance_Values.pkl")
-for idx in data.index:
+# Iterating through the data in dataTable.py.
+for idx in data1.index:
     # First correcting best fit model thing
     colonPosition = (data1["Best_Fit_Model"][idx]).find(":")
     print("This is the position of the colon in the string: " + str(colonPosition))
@@ -31,7 +37,7 @@ for idx in data.index:
         if (data['Frequency'][idx]) == 'DNE':
             data['Frequency'][idx] = 0
         elif float(data['Significance'][idx]) > 3.0:
-            if (float(data['Q Value'][idx]) >= 2.0) and (float(data['Q Value'][idx] <= 30)):
+            if (float(data['Q Value'][idx]) >= 2.0):
                 goodValues.loc[idx,'OBSID'] = data['OBSID'][idx]
                 goodValues.loc[idx,'Q Value'] = data['Q Value'][idx]
                 goodValues.loc[idx,'Significance'] = data['Significance'][idx]
@@ -44,7 +50,7 @@ for idx in data.index:
                 okayValues.loc[idx,'Significance'] = data['Significance'][idx]
                 okayValues.loc[idx,'Frequency'] = data['Frequency'][idx]
                 okayValues.loc[idx,'RMS Value'] = data['RMS Value'][idx]
-                goodValues.loc[idx,'Best Fit Model'] = data1['Best_Fit_Model'][idx]
+                okayValues.loc[idx,'Best Fit Model'] = data1['Best_Fit_Model'][idx]
 
 # Counting the number of items in the dataframe
 print(goodValues)
@@ -57,4 +63,5 @@ print("This is the number of observations in the okayValues dataframe: " + str(o
 goodValues.to_pickle("/Users/rohanpunamiya/Dropbox (GaTech)/CygX2/goodValues.pkl")
 okayValues.to_pickle("/Users/rohanpunamiya/Dropbox (GaTech)/CygX2/okayValues.pkl")
 goodValues.to_excel("/Users/rohanpunamiya/Dropbox (GaTech)/CygX2/goodValues.xlsx")
+data.to_pickle("/Users/rohanpunamiya/Dropbox (GaTech)/CygX2/Q_Significance_Values.pkl")
 
